2a35c1e673ebc1df08184679a682eac4e5d8c295
luislong0 <luisptavio.l.p@gmail.com>
2023-10-12 14:24:37 -0300

    feat: java class 05

---
+++ src/main/java/br/com/longo/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,67 @@
+package br.com.longo.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.longo.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    var servletPath = request.getServletPath();
+    if (servletPath.startsWith("/tasks/")) {
+      // Pegar a autenticação (usuario e senha)
+      var authorization = request.getHeader("Authorization");
+      var user_password = authorization.substring("Basic".length()).trim();
+
+      byte[] authDecode = Base64.getDecoder().decode(user_password);
+
+      var authString = new String(authDecode);
+
+      System.out.println("Authorization: " + authString);
+
+      String[] credentials = authString.split(":");
+      String username = credentials[0];
+      String password = credentials[1];
+
+      System.out.println(username);
+      System.out.println(password);
+
+      // Validar usuário
+      var user = this.userRepository.findByUsername(username);
+      if (user == null) {
+        response.sendError(401, "Usuário sem autorização");
+      } else {
+        // Validar senha
+
+        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+        if (passwordVerify.verified) {
+          request.setAttribute("idUser", user.getId());
+          filterChain.doFilter(request, response);
+        } else {
+          response.sendError(401, "Usuário sem autorização");
+
+        }
+      }
+    } else {
+      // Segue viagem
+          filterChain.doFilter(request, response);
+    }
+  }
+}
